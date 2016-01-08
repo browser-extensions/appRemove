18c70febe0d6159d6ed469444e6d10ebd48917da
zanjs <root@zanjs.com>
2016-01-08 16:01:41 +0800

    :100: By gitpush

--- content_action.js
+++ content_action.js
@@ -33,6 +33,22 @@
 			}
 		},500)
 		
+        
+        // 创建一个简单的文字通知：
+        var notification = webkitNotifications.createNotification(
+        '48.png',  // icon url - can be relative
+        'Hello!',  // notification title
+        'Lorem ipsum...'  // notification body text
+        );
+
+        // 或者创建一个 HTML 通知：
+        var notification = webkitNotifications.createHTMLNotification(
+        'notification.html'  // html url - can be relative
+        );
+
+        // 显示通知
+        notification.show();
+        
 		
 		//taobaoClose.parent().remove();
 		
