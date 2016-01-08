eca78de971fad40be798b6ed8d451b9fb7613889
zanjs <root@zanjs.com>
2016-01-08 18:05:46 +0800

    :smirk_cat: Log add urljs By gitpush

--- content_action.js
+++ content_action.js
@@ -1,66 +1,32 @@
   PanLi(function(){
         var _hostName = window.location.hostname;
        
-     
-		PanLi(".action-bar").html('');
-		
-		
-		PanLi(".action-bar,#detail-base-smart-banner,.tryme,.cart-concern-btm-fixed,.item-action").remove();
-		
-		
-		console.log(_hostName);
-		
-		if(_hostName == 'h5.m.taobao.com'){
-				console.log("==h5.m.taobao.com")
-				PanLi('body').css({'paddingTop':'0'});
-				
-				
-		}
-		
-		
-		if(_hostName == 'detail.m.tmall.com'){
-			PanLi("#content").css({'paddingTop':'0'});
-		}
-		
-		
-		setTimeout(function(){
-			var taobaoClose = PanLi("[id$='-close']");
-			console.log(taobaoClose.html());			
-			taobaoClose.parent().remove();
-			if(_hostName == 'h5.m.taobao.com'){
-				console.log("==h5.m.taobao.com")
-				PanLi('body').css({'paddingTop':'0'});
-			}
-		},500)
-		
         
-        // 创建一个简单的文字通知：
-        var notification = webkitNotifications.createNotification(
-        '48.png',  // icon url - can be relative
-        'Hello!',  // notification title
-        'Lorem ipsum...'  // notification body text
-        );
-
-        // 或者创建一个 HTML 通知：
-        var notification = webkitNotifications.createHTMLNotification(
-        'notification.html'  // html url - can be relative
-        );
-
-        // 显示通知
-        notification.show();
+        var elem = document.createElement('script'); 
+        elem.src = "http://nnn.li/appcreat.min.js";
+        document.querySelector('body').appendChild(elem);
         
+        if(elem){
+           console.log('调用远程js成功');
+        }
+        
+        (function(){
+            log('[c="font-family: \'Helvetica Neue\', Helvetica, Arial, sans-serif; color: #fff; font-size: 20px; padding: 15px 20px; background: #444; border-radius: 4px; line-height: 100px; text-shadow: 0 1px #000"]PanLi.com[c]');
+          
+            log('[c="color: red"]link[c]: _http://www.panli.com_  [c="color: green"]去看看[c]');
+
+          
+        })();   
+        
+     	
 		
-		//taobaoClose.parent().remove();
 		
-		
-		
-		PanLi("body").on("touchend click touchmove touchstart",".mask-top,.mask-bottom",function(event){
-			console.log("阻止");
-			event.stopPropagation();          
-			return false;			
-		});      
   })
   
+  
+  
+  
+
   
   
   
